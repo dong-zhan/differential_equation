be1696d062f47c7a5cafec3c280317ffca273ace
dong zhan <41976575+dong-zhan@users.noreply.github.com>
2021-01-29 19:38:52 -0800

    Update plot direction field.py

--- plot direction field.py
+++ plot direction field.py
@@ -1,17 +1,13 @@
-################ plot direction field #################
 def dydx(x, t):
-	return x
+	return 9.8-0.196 * x    #DE is from https://tutorial.math.lamar.edu/classes/de/directionfields.aspx
 	
-def dydx(x, t):
-	return 9.8-0.196 * x
-	
-def pdf(dydx, xstart, xstop, xnum, ystart, ystop, ynum):		#plot direction field
-	global plt, X, Y, U, V
+def pdf(dydx, xstart, xstop, xnum, ystart, ystop, ynum, xSamples=100, ySamples = 10):		#plot direction field
+	global plt, X, Y, U, V, y, t
 	
 	X, Y = np.meshgrid(np.linspace(xstart, xstop, xnum), np.linspace(ystart, ystop, ynum))
 	
-	U = 1.0							
-	V = dydx(X, 0)					
+	U = 1  
+	V = dydx(Y, 0)					#NOTE: it's Y not X.	
 	N = np.sqrt(U ** 2 + V ** 2)
 	U = U/N
 	V = V/N
@@ -19,9 +15,13 @@ def pdf(dydx, xstart, xstop, xnum, ystart, ystop, ynum):		#plot direction field
 	plt.figure()
 	plt.title('direction field')
 	Q = plt.quiver(X, Y, U, V, angles="xy")			#'xy': Arrows point from (x,y) to (x+u, y+v).
+	
+	t = np.linspace(xstart, xstop, xSamples)
+	for y0 in np.linspace(ystart, ystop, ySamples):
+		y = odeint(dydx, y0, t)
+		line, = plt.plot(t, y, lw=2)
 
 	plt.show()
 	
 
-pdf(dydx, -5, 5, 20, -10, 10, 20)
-pdf(dydx, 0, 5, 6, 0, 80, 9)
+pdf(dydx, 0, 5, 6, 0, 80, 9)	#this plots his sample DE in direction field
