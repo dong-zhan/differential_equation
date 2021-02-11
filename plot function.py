5a44694ec0e2d7044ee4759c6ecaf98219369d96
dong zhan <41976575+dong-zhan@users.noreply.github.com>
2021-02-11 13:18:01 -0800

    Update plot function.py

--- plot function.py
+++ plot function.py
@@ -5,15 +5,16 @@ def df(x):
 def df(x):
 	return np.sin(x)
 
-def pf(df, xstart, xstop, xnum = 50):		
-	global plt, t, s
+def pf(df, xstart, xstop, xnum = 50, show = True):		
+	global plt, t, s, xstep
 	
-	xstep = (xstop-xstart)/(xnum-1)
+	xstep = (xstop-xstart)/(xnum)
 	
-	t = np.arange(xstart, xstop, xstep)
+	t = np.arange(xstart, xstop+xstep, xstep)			#use xstop+xstep to include the endpoint.
 	s = df(t)
-	line, = plt.plot(t, s, lw=2)
-
-	plt.show()
+	line, = plt.plot(t, s)		
+	
+	if show:
+		plt.show()
 	
 pf(df, 0, np.pi*2)
