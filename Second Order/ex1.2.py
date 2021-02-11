741acae263742aefe0ea01a8fd10aad594b2c1c6
dong zhan <41976575+dong-zhan@users.noreply.github.com>
2021-02-11 13:22:13 -0800

    Update ex1.2.py

--- Second Order/ex1.2.py
+++ Second Order/ex1.2.py
@@ -1,5 +1,5 @@
 def df_d2(x):
-	return 9 * np.exp(-3*t) + 9 * np.exp(3*t)
+	return 9 * np.exp(-3*t) + 9 * np.exp(3*t)		#very strange here, I used t instead of x, but, it works...
   
 def df_solution(t):		
 	return np.exp(-3*t) + np.exp(3*t)
